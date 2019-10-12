50b468b734d68b74ff7e2c0579e919f24a2bb8b8
shanazramsaroop <ssramsaroop6835@eagle.fgcu.edu>
2019-10-12 15:37:08 -0400

    added readMe

---
+++ Personal-Project/Hello World.cs
@@ -0,0 +1,177 @@
+﻿using System;
+namespace Personal_Project
+{
+    public class ReadMe
+    {
+        public ReadMe()
+        {
+            Console.WriteLine("C#");
+            Console.WriteLine("");
+            Console.WriteLine("Goals");
+            Console.WriteLine("Complete the C# fundametals for absolute beginners course");
+            Console.WriteLine("");
+            Console.WriteLine("Boundaries/Scope");
+            Console.WriteLine("Complete course intrduction.");
+            Console.WriteLine("Complete understanding namespaces and wokring with .NET class library.");
+            Console.WriteLine("");
+            Console.WriteLine("Success criteria");
+            Console.WriteLine("Create my own program");
+            Console.WriteLine("Log my progress weekly to show my activity");
+            Console.WriteLine("");
+            Console.WriteLine("Constraints");
+            Console.WriteLine("Learning everything from free online sources");
+            Console.WriteLine("");
+            Console.WriteLine("Stakeholders");
+            Console.WriteLine("Professor- grade");
+            Console.WriteLine("Parents- investing in future and success, make them proud");
+            Console.WriteLine("Persepctive employers- will make me more employable after having made my own program");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Timelines");
+            Console.WriteLine("");
+            Console.WriteLine("Week:");
+            Console.WriteLine("");
+            Console.WriteLine("One:");
+            Console.WriteLine("Installing visual studio (6 mins)");
+            Console.WriteLine("Creating your first C# program (17 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Two:");
+            Console.WriteLine("Understanding your first C# program (17 mins)");
+            Console.WriteLine("The if decision statement (24 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Three:");
+            Console.WriteLine("Understanding data types and variables (28 mins)");
+            Console.WriteLine("for interation statement (18 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Four:");
+            Console.WriteLine("Understanding arrays (18 mins)");
+            Console.WriteLine("Defining and calling methods (31 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Five:");
+            Console.WriteLine("While interation statement (18 mins)");
+            Console.WriteLine("Working with date and times (10 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Six:");
+            Console.WriteLine("Working with strings (24 mins)");
+            Console.WriteLine("Understanding classes (24 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Seven:");
+            Console.WriteLine("Understanding scope and accessibility modifiers (17 mins)");
+            Console.WriteLine("More about classes and methods (35 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Eight:");
+            Console.WriteLine("Creating and adding refernces to assemblies (20 mins)");
+            Console.WriteLine("Working wit collections (23 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Nine:");
+            Console.WriteLine("Working with LINQ (24 mins)");
+            Console.WriteLine("Enumerations and the switch decision statement (13 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Ten:");
+            Console.WriteLine("Gracefully handling exceptions (21 mins)");
+            Console.WriteLine("Understanding events and event-driven programming (21 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Eleven:");
+            Console.WriteLine("Where to go from here (33 mins)");
+            Console.WriteLine("Operations, expressions and statements (15 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("Twelve:");
+            Console.WriteLine("Working with code files, projects and solutions (11 mins)");
+            Console.WriteLine("Understanding namespaces and working with .NET class library (18 mins)");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("What I learned in each video:");
+            Console.WriteLine("");
+            Console.WriteLine("Week one:");
+            Console.WriteLine("");
+            Console.WriteLine("Installing Visual Studio");
+            Console.WriteLine("Time spent: 1 hour.");
+            Console.WriteLine("This video showed how to download visual studio and which components to download.");
+            Console.WriteLine("");
+            Console.WriteLine("Creating your first c# program");
+            Console.WriteLine("Time spent: 30 minutes.");
+            Console.WriteLine("In this video, I got familiar with visual studio and ran a simple Hello World code, which printed Hello World!. " +
+                "I also linked and pushed my porject to GitHub.");
+            Console.WriteLine("");
+            Console.WriteLine("Week two:");
+            Console.WriteLine("");
+            Console.WriteLine("Understanding your first c# program");
+            Console.WriteLine("Time spent: 1 hour.");
+            Console.WriteLine("The .NET framework consists of the class library and runtime(aka common language runtime(CLR)). " +
+                "The class library is a library of code and and CLR takes care of low level details and provides a layer of protection for the program. " +
+                "the funtion WriteLine displays text on one line and ReadLine waits for a input from the user. " +
+                "{} represents a code block while a method is a block of code that has a name and a class is a container for all the methods. " +
+                "The Console funtion makes a call into the .NET library. " +
+                "The '.' known as the member accessor which allows you to access a member of the class and () executes the command now. " +
+                "The argument (two double quotation marks) allows data to be outputted and '; ' completes a thought.");
+            Console.WriteLine("");
+            Console.WriteLine("The if statement decision");
+            Console.WriteLine("Time spent: 1 hour.");
+            Console.WriteLine("With the if statement, the output depends on the user's input. " +
+                "The ' = ' is an assignemtn operator while ' == ' is used for equalivalency. " +
+                "The if statement works by evaluating the input againt the condition and if the condition is true, whatever codeblock is directly below, will be executed. " +
+                "'else if' can be used after an is statement, and it evaluates the input again(if the input failed the first evaluation), then it will also execute the codebloc directly below. " +
+                "A scope of a variable is where a variable is accessible. The ' += ' is used to concatenate and it adds one variable to another.");
+            Console.WriteLine("");
+            Console.WriteLine("Week three:");
+            Console.WriteLine("");
+            Console.WriteLine("Understanding data types and variables");
+            Console.WriteLine("Time spent: 1 hour.");
+            Console.WriteLine("In this video, Bob talked about selecting a data type and initializing a variable. " +
+                "He compared a variable to a bucket in which you can put things into, use it, empty it and then reuse it. " +
+                "The data type 'int' is short for integer and represents any whole number in the range of [-2.147 b , 2.147 b]. " +
+                "The assignment operator ' = ' is used to assign a value or word to a variable. " +
+                "C# uses arithmatic operators, such as addition(+), subtraction(-), multiplication(*) and division(/). " +
+                "Two forward slashes '//' are used to comment out code and an asterick and a forward slash is used for a lengthy comment. " +
+                "Ctrl followed by . is used to automatically change all the variable names if you change just one. " +
+                "Camel case is used when naming a variable, so it is more readable, this is where the first letter of the first word is in lower case and the first letter of any other words are in upper case. " +
+                "Intellisense gives a list of possible functions and variable based on what you began to type and can help programmers, since it gives the proper spelling and format. " +
+                "The format for initializing a variable is: variable type + variable name = assignment(word, value, computation...).");
+            Console.WriteLine("");
+            Console.WriteLine("For iteration statement");
+            Console.WriteLine("Time spent: 1 hour.");
+            Console.WriteLine("The for iteration is used to loop/iterate through a sequence of things to find something(successful match). " +
+                "The format for a for iteration is: for(declare variable; condition; change/increment). " +
+                "A break is used to break out of the for iteration. Using a breakpoint can help programmers to debug the program.");
+            Console.WriteLine("");
+            Console.WriteLine("Week four:");
+            Console.WriteLine("Understanding arrays");
+            Console.WriteLine("Time spent: 1 hour.");
+            Console.WriteLine("An array is a sequence/collection of data. " +
+                "The 'Length' function identifies how many items in the array. " +
+                "The Reverse function reverses the order of the array. " +
+                "The foreach statement executes a statement or a block of statements for each element.");
+            Console.WriteLine("");
+            Console.WriteLine("Defining and calling methods");
+            Console.WriteLine("Time spent: 1 hour 30 minutes.");
+            Console.WriteLine("Methods are used to organise code, eliminate duplicate code and take certain features and give it a name. " +
+                "Using methods improves the readability and reduces bloat. " +
+                "Methods are a block of code defined by {} and are defined inside of a class but outside of the previous method. " +
+                "The return statement terminates execution of the method in which it appears and returns control to the calling method.");
+            Console.WriteLine("");
+            Console.WriteLine("Week five:");
+            Console.WriteLine("");
+            Console.WriteLine("While iteration statement");
+            Console.WriteLine("Time spent: 1 hour.");
+            Console.WriteLine("With a for statement, the programmer has to know how many times they need to iterate, " +
+                "whereas a while loop can be used when the programmer does not know, since the loop will run unitl some condition is met. " +
+                "A 'do while' loop allows some code to always be iterated at least one time before breaking out of the iteration statement. " +
+                    "The format for the do wile iteration is do command while condition. The Clear function clears everything that was previously on the screen. " +
+                    "The Random function creates a random value.");
+            Console.WriteLine("");
+            Console.WriteLine("Working with dates and times");
+            Console.WriteLine("Time spent: 30 minutes.");
+            Console.WriteLine("The function DateTime gives the current date and time(in the form mm/dd/yy and HH:MM pm/am). " +
+                "The ToShortDateString function only prints the current date(mm/dd/yy). " +
+                "The ToShortTimeString function gives the current time only (HH:MM pm/am). " +
+                "The ToLongDateString print the date in the form day of week, month date, year. " +
+                "The ToLongTimeString prints the time in the form HH:MM:SS pm/am. " +
+                "The AddDay funtion adds days to the current date and the AddHours adds hours to the current time. " +
+                "To subtract hours or days, use a negative number with the AddHours or AddDays function, respectively. " +
+                "The Month function will give the month as a number and TotalDays gives the number of days between two dates. " +
+                "The Subtract function subtracts the given date from the current date. TimeSpan gives the amount of time between two dates.");
+            Console.ReadLine();
+
+        }
+    }
+}
