2d3f788bba3fefbb2ed25aa09b2208b28cee3586
shanazramsaroop <ssramsaroop6835@eagle.fgcu.edu>
2019-10-12 15:54:48 -0400

    added code

---
+++ Personal-Project/Dates and times.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Personal_Project
+{
+    public class Dates_and_times
+    {
+        public Dates_and_times()
+        {
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
+
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+
+            //Console.WriteLine(myValue.Month);
+
+            //DateTime myBirthday = new DateTime(1999, 1, 20);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myBirthday = DateTime.Parse("1,20,1999");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+
+        }
+    }
+}
