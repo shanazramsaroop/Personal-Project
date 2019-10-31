0675e1612421d6397f627806a5d5e4bd8c56918f
shanazramsaroop <ssramsaroop6835@eagle.fgcu.edu>
2019-10-31 19:18:18 -0400

    added scope and modifiers code

---
+++ Personal-Project/Scope and accessibility mofifiers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Personal_Project
+{
+    public class Scope_and_accessibility_mofifiers
+    {
+        private static string k = "";
+
+        public Scope_and_accessibility_mofifiers()
+        {
+            string j = " ";
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+
+                if (i == 9)
+                {
+                    string l = i.ToString();
+                }
+
+                //Console.WriteLine(l);
+
+            }
+            Console.WriteLine("Outside of the for: " + j);
+            Console.WriteLine("Outside of the for: " + k);
+
+            HelperMethod();
+
+            Car myCar = new Car();
+            myCar.DoSomething();
+
+            Console.ReadLine();
+
+        }
+        static void HelperMethod()
+        {
+
+            Console.WriteLine("Value of k from the HelperMethod(): " + k);
+
+        }
+    }
+
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+
+        private string helperMethod()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ Personal-Project/Scope and modifiers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Personal_Project
+{
+    public class Uniderstanding_classes_and_modifiers
+    {
+        public Uniderstanding_classes_and_modifiers()
+        {
+        }
+    }
+}
