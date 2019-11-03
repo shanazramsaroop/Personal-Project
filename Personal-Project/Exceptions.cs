00f525c8984fe4d6818c88e1de0a685ee75a600b
shanazramsaroop <ssramsaroop6835@eagle.fgcu.edu>
2019-11-03 17:14:10 -0500

    added exception code

---
+++ Personal-Project/Exceptions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Personal_Project
+{
+    public class Exceptions
+    {
+        public Exceptions()
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson22\Exampl.txt");
+                Console.WriteLine(content);
+                Console.ReadLine();
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem");
+                Console.WriteLine("Make sure the name of the file is names correctly: Exampl.txt");
+
+            }
+
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem");
+                Console.WriteLine(@"Make sure the directory C:\Lesson22 exists.");
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Could not fiind file.");
+
+
+            }
+
+            finally
+            {
+                //code to finalize
+                //Setting objects to null
+                //closing database connections
+                Console.WriteLine("Closing application now ... ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
