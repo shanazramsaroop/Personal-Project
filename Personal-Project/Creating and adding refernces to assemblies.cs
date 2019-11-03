725c53706eefcf3755f3d95bc0efd7549a509473
shanazramsaroop <ssramsaroop6835@eagle.fgcu.edu>
2019-11-03 01:22:10 -0400

    added reference code

---
+++ Personal-Project/Creating and adding refernces to assemblies.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Personal_Project
+{
+    public class Creating_and_adding_refernces_to_assemblies
+    {
+        public Creating_and_adding_refernces_to_assemblies()
+        {
+
+            public string ScrapeWebpage(string url)
+            {
+                return GetWebpage(url);
+            }
+
+            public string ScrapeWebpage(string url, string filepath)
+            {
+
+                string reply = GetWebpage(url);
+
+                File.WriteAllText(filepath, reply);
+                return reply;
+            }
+
+            private string GetWebpage(string url)
+            {
+
+                WebClient Client = new WebClient();
+                string reply = Client.DownloadString(url);
+
+            }
+        }
+}
